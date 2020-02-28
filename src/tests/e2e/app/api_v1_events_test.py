405f27eb135994a551c6e00da3284b9ce8624884
txemac <txema.bermudez@gmail.com>
2020-02-28 16:57:43 +0100

    Improve e2e tests.

--- src/tests/e2e/app/api_v1_events_test.py
+++ src/tests/e2e/app/api_v1_events_test.py
@@ -9,17 +9,30 @@ def test_get_events_none(client, mocked_requests_get, xml):
     assert response.json() == []
 
 
-@pytest.mark.parametrize('start_date, end_date, offline, expected',
-                         [('1982-04-25', '1983-04-25', 'false', 0),
-                          ('2019-06-30', '2019-06-30', 'false', 1),
-                          ('2019-04-21', '2019-04-23', 'false', 0),
-                          ('2019-04-21', '2019-04-23', 'true', 1),
-                          ('2019-07-29', '2019-08-01', 'false', 1),
-                          ('2019-06-30', '2019-08-30', 'false', 2),
-                          ('2019-03-30', '2019-09-30', 'false', 2),
-                          ('2019-03-30', '2019-09-30', 'true', 3)])
-def test_get_events_ok(client, mocked_requests_get, xml, start_date, end_date, offline, expected):
+@pytest.mark.parametrize('url, expected',
+                         [(f"/api/v1/events", 2),
+                          (f"/api/v1/events?offline=false", 2),
+                          (f"/api/v1/events?offline=true", 3),
+                          (f"/api/v1/events?start_date=2010-01-01", 2),
+                          (f"/api/v1/events?start_date=2010-01-01&offline=false", 2),
+                          (f"/api/v1/events?start_date=2019-07-01&offline=false", 1),
+                          (f"/api/v1/events?start_date=2010-01-01&offline=true", 3),
+                          (f"/api/v1/events?start_date=2019-07-01&offline=true", 1),
+                          (f"/api/v1/events?end_date=2010-01-01", 0),
+                          (f"/api/v1/events?end_date=2010-01-01&offline=false", 0),
+                          (f"/api/v1/events?end_date=2019-07-01&offline=false", 1),
+                          (f"/api/v1/events?end_date=2010-01-01&offline=true", 0),
+                          (f"/api/v1/events?end_date=2019-07-01&offline=true", 2),
+                          (f"/api/v1/events?start_date=1982-04-25&end_date=1983-04-25&offline=false", 0),
+                          (f"/api/v1/events?start_date=2019-06-30&end_date=2019-06-30&offline=false", 1),
+                          (f"/api/v1/events?start_date=2019-04-21&end_date=2019-04-23&offline=false", 0),
+                          (f"/api/v1/events?start_date=2019-04-21&end_date=2019-04-23&offline=true", 1),
+                          (f"/api/v1/events?start_date=2019-07-29&end_date=2019-08-01&offline=false", 1),
+                          (f"/api/v1/events?start_date=2019-06-30&end_date=2019-08-30&offline=false", 2),
+                          (f"/api/v1/events?start_date=2019-03-30&end_date=2019-09-30&offline=false", 2),
+                          (f"/api/v1/events?start_date=2019-03-30&end_date=2019-09-30&offline=true", 3)])
+def test_get_events_ok(client, mocked_requests_get, xml, url, expected):
     mocked_requests_get.return_value.text = xml
-    response = client.get(f"/api/v1/events?start_date={start_date}&end_date={end_date}&offline={offline}")
+    response = client.get(url)
     assert response.status_code == HTTP_200_OK
     assert len(response.json()) == expected
