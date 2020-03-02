34b408922e2faecb4c4dc17a010b17de163cce45
txemac <txema.bermudez@gmail.com>
2020-03-02 23:14:53 +0100

    Change the GET API endpoint to call database.

--- src/app/api_v1_events.py
+++ src/app/api_v1_events.py
@@ -1,22 +1,25 @@
 from datetime import date
-from datetime import datetime
 from typing import List
+from typing import Optional
 
 from fastapi import APIRouter
+from fastapi import Depends
+from sqlalchemy.orm import Session
 from starlette.status import HTTP_200_OK
 
-from data import events
-from data import external_api_client
-from data.schemas import BaseEventCreate
+from data.database import get_db
+from data.database.base_event import BaseEvent
+from data.schemas import BaseEventDB
 
 api_v1_events = APIRouter()
 
 
-@api_v1_events.get('/', response_model=List[BaseEventCreate], status_code=HTTP_200_OK)
+@api_v1_events.get('/', response_model=List[BaseEventDB], status_code=HTTP_200_OK)
 def get_events(
-        start_date: date = datetime.min.date(),
-        end_date: date = datetime.max.date(),
-        offline: bool = False
+        *,
+        db_session: Session = Depends(get_db),
+        start_date: Optional[date] = None,
+        end_date: Optional[date] = None,
+        offline: Optional[bool] = False,
 ):
-    base_events = external_api_client.get_base_events()
-    return events.get_events(events=base_events, start_date=start_date, end_date=end_date, offline=offline)
+    return BaseEvent.get_events(db_session=db_session, start_date=start_date, end_date=end_date, offline=offline)
