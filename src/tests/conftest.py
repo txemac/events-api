54150d1b49b7eb7df18d1ea2b0d33eec68117b42
txemac <txema.bermudez@gmail.com>
2020-03-02 14:02:13 +0100

    Add postgreSLQ database. Improve dockerfile and docker-compose. Add new fixtures for pytests. Init db.

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,8 +1,17 @@
+import os
+
 import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy_utils import create_database
+from sqlalchemy_utils import database_exists
+from sqlalchemy_utils import drop_database
 from starlette.testclient import TestClient
 
 from app.main import app
 from data import external_api_client
+from data.database import Base
+from data.database import get_db
 from data.schemas import BaseEvent
 from data.schemas import Event
 from data.schemas import Zone
@@ -12,6 +21,45 @@ from data.schemas import Zone
 def client():
     with TestClient(app) as client:
         yield client
+
+
+url = f'{os.getenv("DATABASE_URL")}_test'
+_db_conn = create_engine(url)
+
+
+def get_test_db_conn():
+    assert _db_conn is not None
+    return _db_conn
+
+
+def get_test_db():
+    sess = Session(bind=_db_conn)
+
+    try:
+        yield sess
+    finally:
+        sess.close()
+
+
+@pytest.fixture(scope="session", autouse=True)
+def database():
+    if database_exists(url):
+        drop_database(url)
+    create_database(url)
+    Base.metadata.create_all(_db_conn)
+    app.dependency_overrides[get_db] = get_test_db
+    yield
+    drop_database(url)
+
+
+@pytest.yield_fixture
+def session():
+    db_session = Session(bind=_db_conn)
+
+    yield db_session
+    for tbl in reversed(Base.metadata.sorted_tables):
+        _db_conn.execute(tbl.delete())
+    db_session.close()
 
 
 @pytest.fixture
