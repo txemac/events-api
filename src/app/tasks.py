65a53aeceffb05cf1c342b377800ea563c6fdba2
txemac <txema.bermudez@gmail.com>
2020-03-05 13:51:15 +0100

    Add scheduler to read feed.

--- src/app/tasks.py
+++ src/app/tasks.py
@@ -2,6 +2,6 @@ from data.api_client import feed_api_client
 from data.database import BaseEvent
 
 
-def job_read_feed(db_session):
+def job_read_feed():
     for base_event in feed_api_client.get_feed():
         BaseEvent.create_or_update(base_event=base_event)
--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 from typing import Dict
 
+from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask
+from pytz import utc
 
 from app import responder
 from app.api_v1_events import api_v1_events
+from app.tasks import job_read_feed
 from data.database import Base
 from data.database import engine
 
@@ -14,6 +17,12 @@ def create_app() -> Flask:
     api = Flask(__name__)
 
     api.register_blueprint(api_v1_events)
+
+    # scheduler
+    scheduler = BackgroundScheduler()
+    scheduler.configure(timezone=utc)
+    scheduler.add_job(job_read_feed, 'interval', minutes=5)
+    scheduler.start()
 
     return api
 
